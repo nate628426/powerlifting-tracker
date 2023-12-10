1c6bb02bc109b373dbe4310d5b1c690ef7e02a33
Nathan <58781334+nate628426@users.noreply.github.com>
2023-12-10 17:29:36 -0500

    Final copies of files from local drive
    Overwrite previous renditions of code

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-//TODO: Finish work on updateMessage function
 document.getElementById('add-button').addEventListener('click', handleAddButton);
 document.getElementById('edit-button').addEventListener('click', handleEditButton);
-const script_result = document.getElementById('message1');
+document.getElementById('search-button').addEventListener('click', handleSearchButton);
+document.getElementById('delete-button').addEventListener('click', handleDeleteButton);
 
+const script_result = document.getElementById('message1');
+//For Adding information to MongoDB Database
 async function handleAddButton() {
+    //prevent page reload
+    event.preventDefault();
+    //Get HTML form data to put into object
+    const idValue = document.getElementById('_id').value;
     const machineValue = document.getElementById('machine1').value;
     const exerciseValue = document.getElementById('exercise1').value;
     const setsValue = document.getElementById('sets1').value;
     const durationValue = document.getElementById('duration1').value;
     const commentsValue = document.getElementById('comments1').value;
-
+    //Requires ID Value, unique.
+    if (idValue === '') {
+        updateMessage('Failed to add exercise! _id is required.', false);
+        return;
+    }
     try {
-        updateMessage('Successfully added a new exercise!', true); //only shows briefly
+        //send data to postData to send to server 
+        updateMessage('Successfully added a new exercise!', true); 
         await postData('http://localhost:3000/api/exercise', {
+            _id: idValue,
             machine: machineValue,
             exercise: exerciseValue,
             sets: setsValue,
@@ -26,17 +38,22 @@ async function handleAddButton() {
         console.error(error)
     }
 }
-
+//Edit Data in MongoDB database, identical structure to handleAddButton()
 async function handleEditButton() {
-    const idValue = document.getElementById('id1').value;
+    event.preventDefault();
+    const idValue = document.getElementById('_id').value;
     const machineValue = document.getElementById('machine1').value;
     const exerciseValue = document.getElementById('exercise1').value;
     const setsValue = document.getElementById('sets1').value;
     const durationValue = document.getElementById('duration1').value;
     const commentsValue = document.getElementById('comments1').value;
 
+    if (idValue === '') {
+        updateMessage('Failed to edit exercise! _id is required.', false);
+        return;
+    }
+
     try {
-        
         await postData('http://localhost:3000/api/exercise', {
             _id: idValue,
             machine: machineValue,
@@ -46,49 +63,134 @@ async function handleEditButton() {
             comments: commentsValue,
         });
         updateMessage('Successfully edited exercise!', true);
-        
     }
     catch (error) {
         updateMessage('Failed to edit exercise!', false);
         console.error(error)
     }
 }
+//Search for information in MongoDB Database
+async function handleSearchButton(event) {
+    event.preventDefault();
+    const idValue = document.getElementById('_id').value;
+    const machineValue = document.getElementById('machine1').value;
+    const exerciseValue = document.getElementById('exercise1').value;
+    const setsValue = document.getElementById('sets1').value;
+    const durationValue = document.getElementById('duration1').value;
 
+    // Create an object for the query
+    let query = {};
+
+    // Only add properties to the query if their corresponding input values are not empty
+    if (idValue !== '') query._id = idValue;
+    if (machineValue !== '') query.machine = machineValue;
+    if (exerciseValue !== '') query.exercise = exerciseValue;
+    if (setsValue !== '') query.sets = setsValue;
+    if (durationValue !== '') query.duration = durationValue;
+
+    try {
+        //send to different API URL than add/edit.
+        const response = await postData('http://localhost:3000/api/search', query);
+        const data = await response.json(); // Parse JSON response body
+        updateMessage('Search completed!', true);
+        displaySearchResults(data); // Pass parsed data to displaySearchResults
+    }
+    catch (error) {
+        updateMessage('Failed to complete search!', false);
+        console.error(error)
+    }
+}
+//Delete data from MongoDB Database
+async function handleDeleteButton(event) {
+    event.preventDefault();
+    //Only takes in _id, so required field.
+    const idValue = document.getElementById('delete_id').value;
+    if (idValue === '') {
+        updateMessage('Failed to delete exercise! _id is required.', false);
+        return;
+    }
+    try {
+        //Different API URL
+        await postData('http://localhost:3000/api/delete', { _id: idValue });
+        updateMessage('Successfully deleted exercise!', true);
+    }
+    catch (error) {
+        updateMessage('Failed to delete exercise!', false);
+        console.error(error)
+    }
+}
+//Dynamically display search results from MongoDB Database
+function displaySearchResults(results) {
+    const resultsContainer = document.getElementById('search-results');
+    if (!resultsContainer) {
+        console.error('Could not find results container');
+        return;
+    }
+    resultsContainer.innerHTML = ''; // Clear previous results
+    if (!Array.isArray(results)) {
+        console.error('Results is not an array');
+        return;
+    }
+    results.forEach(result => {
+        const resultDiv = document.createElement('div');
+        
+        // Create a formatted HTML representation of the result
+        const resultHTML = `
+            <p><strong>ID:</strong> ${result._id}</p>
+            <p><strong>Machine:</strong> ${result.machine}</p>
+            <p><strong>Exercise:</strong> ${result.exercise}</p>
+            <p><strong>Sets:</strong> ${result.sets}</p>
+            <p><strong>Duration:</strong> ${result.duration}</p>
+        `;
+        
+        resultDiv.innerHTML = resultHTML;
+        resultsContainer.appendChild(resultDiv);
+    });
+}
+//Add data to MongoDB Database, but this time enforce/validate data type through Mongoose ODM technology.
+async function handleStrictAddButton(event) {
+    event.preventDefault();
+    const coach_id = document.getElementById('coach_id').value;
+    const machine = document.getElementById('strict_machine').value;
+    const exercise = document.getElementById('strict_exercise').value;
+    const sets = document.getElementById('strict_sets').value;
+    const duration = document.getElementById('strict_duration').value;
+    const comments = document.getElementById('strict_comments').value;
+
+    const data = { _id: coach_id, machine, exercise, sets, duration, comments };
+
+    try {
+        //New API URL
+        await postData('http://localhost:3000/api/strict-add', data);
+        updateMessage('Successfully strictly added exercise!', true);
+    }
+    catch (error) {
+        updateMessage('Failed to strictly add exercise!', false);
+        console.error(error)
+    }
+}
+//Send data to server for modifying information in MongoDB database
 async function postData(url, data) {
     try {
         const response = await fetch(url, {
             method: 'POST',
             headers: {
-            'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response; 
+    } catch (error) {
+        console.error('Error:', error);
     }
-    catch (error) {
-        console.error(error);
-    }
-    finally {
-        // if (!response.ok) {
-        //     throw new Error(`Network response was not ok. Status:${response.status}`);
-        // }
-        // console.log("Response from server: " + response.status);
-        // return await response.json();
-        console.log("Request sent to server")
-    }
-    
 }
-//Not working yet
+//Validation message for front-end depending on action performed.
 function updateMessage(message, isSuccess) {
-    // const messageElement = document.getElementById('message1');
-    // messageElement.textContent = message;
-    // if (isSuccess) {
-    //     messageElement.style.color = 'green'; // Style for success
-    // } else {
-    //     messageElement.style.color = 'red'; // Style for error
-    // }
-    // console.log("function updateMessage called")
-    script_result.innerHTML = message;
-    console.log("function updateMessage called!")
+    script_result.textContent = message;
+    script_result.className = isSuccess ? 'success' : 'error';
 }
 
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// ToDO: description of code, perhaps include once modifier to close connection to MongoDB Atlas more often
 const express = require('express');
+const mongoose = require('mongoose')
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://admin1:C8j2ESqnoEXVKlWL@cs348-test1.oaamzwv.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
@@ -7,16 +7,30 @@ const PORT = 3000;
 const cors = require('cors');
 app.use(cors());
 
+//Mongoose schema for adding data, ODM technology.
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }); 
+
+const strictExerciseSchema = new mongoose.Schema({
+    _id: { type: String, required: true },
+    machine: { type: String, required: true },
+    exercise: { type: String, required: true },
+    sets: { type: Number, required: true },
+    duration: { type: Number, required: true },
+    comments: { type: String, required: true }
+}, { strict: true });
+
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: '1',
         strict: true,
         deprecationErrors: true,
-}
+    }
 });
 
 app.use(express.json());
 
+//Add or Edit data in database.
 app.post('/api/exercise', async (req, res) => {
     console.log("Received POST request");
     const data = req.body;
@@ -31,21 +45,94 @@ app.post('/api/exercise', async (req, res) => {
             // If _id is present in data, edit the document with that _id
             await collection.updateOne({ _id: data._id }, { $set: data }, { upsert: true });
         } else {
-            // If _id is not present, we insert a new document
+            // If _id is not present, insert a new document
             await collection.insertOne(data);
         }
         res.status(200).json({ status: "success, exercise processed successfully" });
         console.log("Exercise processed successfully by MongoDB Atlas");
-        // res.status(200).send("Exercise processed successfully");
     } catch (err) {
         console.error(err);
         res.status(500).send("Failed to process exercise");
     } finally {
-        //not closing properly
         await client.close();
         console.log("Connection to MongoDB Atlas closed?");
     }
 });
+
+//Mongoose ODM, add data through model and new object newExercise
+const StrictExercise = mongoose.model('StrictExercise', strictExerciseSchema, 'recorded_exercises');
+
+app.post('/api/strict-add', async (req, res) => {
+    console.log("Received strict add request");
+    const data = req.body;
+    console.log(data);
+    //create new object
+    const newExercise = new StrictExercise(data);
+
+    try {
+        await client.connect();
+        client.on('error', (error) => {console.error("Error connecting to MongoDB Atlas\n", error)});
+        client.on('open', () => {console.log("Connected to MongoDB Atlas")});
+        const collection = client.db("exercise_data").collection("recorded_exercises");
+        await collection.insertOne(newExercise);
+        res.status(200).json({ status: "success, exercise added successfully" });
+        console.log("Exercise added successfully by MongoDB Atlas");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Failed to add exercise");
+    } finally {
+        await client.close();
+        console.log("Connection to MongoDB Atlas closed?");
+    }
+});
+//Search database for given input filters
+app.post('/api/search', async (req, res) => {
+    console.log("Received search request");
+    const data = req.body;
+    console.log(data);
+
+    try {
+        await client.connect();
+        client.on('error', (error) => {console.error("Error connecting to MongoDB Atlas\n", error)});
+        client.on('open', () => {console.log("Connected to MongoDB Atlas")});
+        const collection = client.db("exercise_data").collection("recorded_exercises");
+        //Collect results from collection.find()
+        const searchResult = await collection.find(data).toArray();
+        res.status(200).json(searchResult);
+        console.log("Search completed successfully by MongoDB Atlas");
+        console.log(searchResult)
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Failed to complete search");
+    } finally {
+        await client.close();
+        console.log("Connection to MongoDB Atlas closed?");
+    }
+});
+//Delete specific object from MongoDB database.
+app.post('/api/delete', async (req, res) => {
+    console.log("Received delete request");
+    const data = req.body;
+    console.log(data);
+
+    try {
+        await client.connect();
+        client.on('error', (error) => {console.error("Error connecting to MongoDB Atlas\n", error)});
+        client.on('open', () => {console.log("Connected to MongoDB Atlas")});
+        const collection = client.db("exercise_data").collection("recorded_exercises");
+        await collection.deleteOne({ _id: data._id });
+        res.status(200).json({ status: "success, exercise deleted successfully" });
+        console.log("Exercise deleted successfully by MongoDB Atlas");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Failed to delete exercise");
+    } finally {
+        await client.close();
+        console.log("Connection to MongoDB Atlas closed?");
+    }
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
